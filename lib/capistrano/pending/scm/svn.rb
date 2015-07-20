016b3b47962ba8b4b9a4f00cf73e618f20586a58
Johannes Leers <johannes@leers.eu>
2015-07-20 11:35:14 +0200

    some more bugfixes

--- lib/capistrano/pending/scm/svn.rb
+++ lib/capistrano/pending/scm/svn.rb
@@ -7,13 +7,13 @@ module Capistrano
         command :svn
 
         def log(from, to)
-          to = 'HEAD' if to == 'master'
-          svn :log, "-r #{from}:#{to}"
+          to = :HEAD if to == :master
+          svn :log, "-r#{from}:#{to}"
         end
 
         def diff(from, to)
-          to = 'HEAD' if to == 'master'
-          svn :diff, "-r #{from}:#{to}"
+          to = :HEAD if to == :master
+          svn :diff, "-r#{from}:#{to}"
         end
       end
     end
